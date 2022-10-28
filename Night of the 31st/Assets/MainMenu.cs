c3d61e6276a9db248fc40873783a243562f539a8
jasxaxp <70676153+jasxaxp@users.noreply.github.com>
2022-10-28 17:15:58 -0400

    quit game

--- Night of the 31st/Assets/MainMenu.cs
+++ Night of the 31st/Assets/MainMenu.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame ()
-{
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-}
+    }
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 
 }
