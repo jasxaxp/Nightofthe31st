c639d1f418af1baf371e9b92dad8d8feae5ef885
jasxaxp <70676153+jasxaxp@users.noreply.github.com>
2022-10-29 00:34:12 -0400

    chsnge

--- Night of the 31st/Assets/MainStory.cs
+++ Night of the 31st/Assets/MainStory.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagment;
+using UnityEngine.SceneManagement;
 
 public class MainStory : MonoBehaviour
 {
