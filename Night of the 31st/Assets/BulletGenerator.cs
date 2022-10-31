16e51508e696109486dab7ef2a1a0579e1d908c4
isaborras <100324706+isaborras@users.noreply.github.com>
2022-10-31 18:07:13 -0400

    updates?

--- Night of the 31st/Assets/BulletGenerator.cs
+++ Night of the 31st/Assets/BulletGenerator.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class BulletGenerator : MonoBehaviour
 {
+	float bulletSpeed = 3000;
+	public GameObject bullet;
+	public bool Firing = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+		{
+			if (Firing == false)
+			{
+				StartCoroutine(Fire());
+			}
+		}
     }
+	
+	IEnumerator Fire()
+	{
+		Firing = true;
+		GameObject tempBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+		Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
+		tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+		Destroy(tempBullet, 1.5f);
+		yield return new WaitForSeconds(0.31f);
+		Firing = false;
+	}
 }
