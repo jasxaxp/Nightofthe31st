7be0f4b0ec902014664bda0f79f928a607cb7863
jasxaxp <70676153+jasxaxp@users.noreply.github.com>
2022-11-01 16:57:00 -0400

    pumpkin

--- Night of the 31st/Assets/TriggerEnemy.cs
+++ Night of the 31st/Assets/TriggerEnemy.cs
@@ -10,7 +10,43 @@ public class TriggerEnemy : MonoBehaviour
 	public GameObject enemy2;
 	public GameObject enemy3;
 	public GameObject enemy4;
+	public GameObject enemy6;
 	public GameObject enemy5;
+	public GameObject enemy7;
+	public GameObject enemy8;
+	public GameObject enemy9;
+	public GameObject enemy10;
+	public GameObject enemy11;
+	public GameObject enemy12;
+	public GameObject enemy13;
+	public GameObject enemy14;
+	public GameObject enemy15;
+	public GameObject enemy16;
+	public GameObject enemy17;
+	public GameObject enemy18;
+	public GameObject enemy19;
+    public GameObject enemy20;
+	public GameObject enemy21;
+	public GameObject enemy22;
+	public GameObject enemy23;
+	public GameObject enemy24;
+	public GameObject enemy25;
+	public GameObject enemy26;
+	public GameObject enemy27;
+	public GameObject enemy28;
+    public GameObject enemy29;
+	public GameObject enemy30;
+	public GameObject enemy31;
+	public GameObject enemy32;
+	public GameObject enemy33;
+	public GameObject enemy34;
+	public GameObject enemy35;
+	public GameObject enemy36;
+	public GameObject enemy37;
+	public GameObject enemy38;
+	public GameObject enemy39;
+	public GameObject enemy40;
+
 	
     // Start is called before the first frame update
     void Start()
@@ -33,5 +69,41 @@ public class TriggerEnemy : MonoBehaviour
 		enemy3.GetComponent<ChasingPumpking>().enabled = true;
 		enemy4.GetComponent<ChasingPumpking>().enabled = true;
 		enemy5.GetComponent<ChasingPumpking>().enabled = true;
+		enemy6.GetComponent<ChasingPumpking>().enabled = true;
+		enemy7.GetComponent<ChasingPumpking>().enabled = true;
+		enemy8.GetComponent<ChasingPumpking>().enabled = true;
+		enemy9.GetComponent<ChasingPumpking>().enabled = true;
+		enemy10.GetComponent<ChasingPumpking>().enabled = true;
+		enemy11.GetComponent<ChasingPumpking>().enabled = true;
+		enemy12.GetComponent<ChasingPumpking>().enabled = true;
+		enemy13.GetComponent<ChasingPumpking>().enabled = true;
+		enemy14.GetComponent<ChasingPumpking>().enabled = true;
+		enemy15.GetComponent<ChasingPumpking>().enabled = true;
+		enemy16.GetComponent<ChasingPumpking>().enabled = true;
+		enemy17.GetComponent<ChasingPumpking>().enabled = true;
+		enemy18.GetComponent<ChasingPumpking>().enabled = true;
+		enemy19.GetComponent<ChasingPumpking>().enabled = true;
+		enemy20.GetComponent<ChasingPumpking>().enabled = true;
+		enemy21.GetComponent<ChasingPumpking>().enabled = true;
+		enemy22.GetComponent<ChasingPumpking>().enabled = true;
+		enemy23.GetComponent<ChasingPumpking>().enabled = true;
+		enemy24.GetComponent<ChasingPumpking>().enabled = true;
+		enemy25.GetComponent<ChasingPumpking>().enabled = true;
+		enemy25.GetComponent<ChasingPumpking>().enabled = true;
+		enemy26.GetComponent<ChasingPumpking>().enabled = true;
+		enemy27.GetComponent<ChasingPumpking>().enabled = true;
+		enemy29.GetComponent<ChasingPumpking>().enabled = true;
+		enemy30.GetComponent<ChasingPumpking>().enabled = true;
+		enemy31.GetComponent<ChasingPumpking>().enabled = true;
+		enemy32.GetComponent<ChasingPumpking>().enabled = true;
+		enemy33.GetComponent<ChasingPumpking>().enabled = true;
+		enemy34.GetComponent<ChasingPumpking>().enabled = true;
+		enemy35.GetComponent<ChasingPumpking>().enabled = true;
+		enemy36.GetComponent<ChasingPumpking>().enabled = true;
+		enemy37.GetComponent<ChasingPumpking>().enabled = true;
+		enemy38.GetComponent<ChasingPumpking>().enabled = true;
+		enemy39.GetComponent<ChasingPumpking>().enabled = true;
+		enemy40.GetComponent<ChasingPumpking>().enabled = true;
+
  	}
 }
