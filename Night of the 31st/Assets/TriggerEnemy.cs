fc86cc5ca32f3e08f9428a032afe2f1784e0f00e
isaborras <100324706+isaborras@users.noreply.github.com>
2022-10-31 21:55:44 -0400

    enemies

--- Night of the 31st/Assets/TriggerEnemy.cs
+++ Night of the 31st/Assets/TriggerEnemy.cs
@@ -6,6 +6,11 @@ public class TriggerEnemy : MonoBehaviour
 {
 	public GameObject player;
 	public GameObject enemy;
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+	public GameObject enemy4;
+	public GameObject enemy5;
 	
     // Start is called before the first frame update
     void Start()
@@ -23,5 +28,10 @@ public class TriggerEnemy : MonoBehaviour
 	{
 		// enable script on GameObject when player enters trigger
 		enemy.GetComponent<ChasingPumpking>().enabled = true;
+		enemy1.GetComponent<ChasingPumpking>().enabled = true;
+		enemy2.GetComponent<ChasingPumpking>().enabled = true;
+		enemy3.GetComponent<ChasingPumpking>().enabled = true;
+		enemy4.GetComponent<ChasingPumpking>().enabled = true;
+		enemy5.GetComponent<ChasingPumpking>().enabled = true;
  	}
 }
