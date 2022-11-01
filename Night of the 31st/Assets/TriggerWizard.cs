5382d7806809c4feeb064c0ba33592f9dadad3cb
isaborras <100324706+isaborras@users.noreply.github.com>
2022-11-01 11:59:32 -0400

    update

---
+++ Night of the 31st/Assets/TriggerWizard.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWizard : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject enemy;
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+	public GameObject enemy4;
+	public GameObject enemy5;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		// enable script on GameObject when player enters trigger
+		enemy.GetComponent<ChasingWizard>().enabled = true;
+		enemy1.GetComponent<ChasingWizard>().enabled = true;
+		enemy2.GetComponent<ChasingWizard>().enabled = true;
+		enemy3.GetComponent<ChasingWizard>().enabled = true;
+		enemy4.GetComponent<ChasingWizard>().enabled = true;
+		enemy5.GetComponent<ChasingWizard>().enabled = true;
+ 	}
+}
