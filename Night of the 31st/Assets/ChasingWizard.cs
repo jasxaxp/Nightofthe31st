0caecf00cdae2d7cda36f1993f5eef9e1af39f76
isaborras <100324706+isaborras@users.noreply.github.com>
2022-11-01 14:05:20 -0400

    wizard

--- Night of the 31st/Assets/ChasingWizard.cs
+++ Night of the 31st/Assets/ChasingWizard.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChasingWizard : MonoBehaviour
 {
+    NavMeshAgent Enemy;
+	GameObject Target;
     // Start is called before the first frame update
     void Start()
     {
-        
+       Target = GameObject.FindWithTag("Player");
+	   Enemy = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Enemy.SetDestination(Target.transform.position);
     }
 }
