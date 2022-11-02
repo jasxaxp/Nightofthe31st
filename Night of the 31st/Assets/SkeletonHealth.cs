dbfe4cb4a17e36fc2b636c5f86518761fcf01bd0
isaborras <100324706+isaborras@users.noreply.github.com>
2022-11-01 21:31:25 -0400

    almost there

--- Night of the 31st/Assets/SkeletonHealth.cs
+++ Night of the 31st/Assets/SkeletonHealth.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
 public class SkeletonHealth : MonoBehaviour
 {
+	public int health = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,23 @@ public class SkeletonHealth : MonoBehaviour
     {
         
     }
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.transform.tag == "Bullet")
+		{
+			if (health <= 0)
+			{
+				Destroy(gameObject);
+				ScoreScript.scoreValue += 1;
+				this.gameObject.GetComponent<AudioSource>().enabled = false;
+				this.gameObject.transform.GetChild(2).gameObject.SetActive(false);
+				this.gameObject.GetComponent<ChasingSkeleton>().enabled = false;
+				this.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+			}
+			else
+			{
+				health -= 1;
+			}
+		}
+	}
 }
